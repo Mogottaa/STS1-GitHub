a39187c0f342f97ed5f047530dcd8271a95e68e6
Sorasilver <101389671+Mogottaa@users.noreply.github.com>
2022-03-30 14:57:46 +0200

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -52,6 +52,51 @@ int plusGrand(int a, int b, int c)
 
 //Fonctions réalisées par l'étudiant 2 :
 
+bool estPair(int nb)
+{
+	if (nb % 2 == 0)
+	{
+		return(true);
+	}
+	else
+	{
+		return(false);
+	}
+}
+
+int sommeNombre(int nb)
+{
+    int resultat=0;
+
+	for (int i=nb; i > 0; i--)
+	{
+        resultat = resultat + i;
+	}
+	return (resultat);
+}
+
+float salaireNet(int salaire)
+{
+    int resultat;
+    resultat=salaire * 1,23;
+    return(resultat);
+}
+
+int plusPetit(int a, int b, int c)
+{
+    if(a < b && a < c)
+    {
+        return(a);
+    }
+    else if(b < a && b < c)
+    {
+        return(b);
+    }
+    else
+    {
+        return(c);
+    }
+}
 
 
 
